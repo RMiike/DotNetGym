fc9b0d104ea9dfc5d91a0445589969ecb6b8e759
Renato Miike Alves <rmalves2@stefanini.com>
2020-10-22 23:32:37 -0300

    adicionado controller unidades basica

--- src/services/DotNetGym.API/Controllers/v1/UnidadesController.cs
+++ src/services/DotNetGym.API/Controllers/v1/UnidadesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DNG.API.Entities;
+using DNG.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DNG.API.Controllers.v1
 {
@@ -6,5 +10,38 @@ namespace DNG.API.Controllers.v1
     [ApiController]
     public class UnidadesController : ControllerBase
     {
+        private readonly DNGDbContext _context;
+        public UnidadesController(DNGDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var unidades = _context.Unidades.AsNoTracking().ToList();
+            return Ok(unidades);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var unidade = _context.Unidades.Where(x => x.Id == id).FirstOrDefault();
+
+            if( unidade == null )
+                return NotFound();
+
+            return Ok(unidade);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Unidade unidade)
+        {
+            _context.Unidades.Add(unidade);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
     }
 }
