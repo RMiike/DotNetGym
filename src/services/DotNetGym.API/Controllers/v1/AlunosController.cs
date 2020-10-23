d25fcda56e4aa8560f583c38f27847acfd607c1a
Renato Miike Alves <rmalves2@stefanini.com>
2020-10-22 23:39:00 -0300

    add 2 controllers

--- src/services/DotNetGym.API/Controllers/v1/AlunosController.cs
+++ src/services/DotNetGym.API/Controllers/v1/AlunosController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DNG.API.Entities;
+using DNG.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DNG.API.Controllers.v1
 {
@@ -6,6 +10,39 @@ namespace DNG.API.Controllers.v1
     [ApiController]
     public class AlunosController : ControllerBase
     {
+        private readonly DNGDbContext _context;
+        public AlunosController(DNGDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var alunos = _context.Alunos.AsNoTracking().ToList();
+            return Ok(alunos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var aluno = _context.Alunos.Where(x => x.Id == id).FirstOrDefault();
+
+            if( aluno == null )
+                return NotFound();
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Aluno aluno)
+        {
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
 
     }
 }
--- src/services/DotNetGym.API/Controllers/v1/ProfessoresController.cs
+++ src/services/DotNetGym.API/Controllers/v1/ProfessoresController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DNG.API.Entities;
+using DNG.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DNG.API.Controllers.v1
 {
@@ -6,5 +10,38 @@ namespace DNG.API.Controllers.v1
     [ApiController]
     public class ProfessoresController : ControllerBase
     {
+        private readonly DNGDbContext _context;
+        public ProfessoresController(DNGDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var professores = _context.Professores.AsNoTracking().ToList();
+            return Ok(professores);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var professor = _context.Professores.Where(x => x.Id == id).FirstOrDefault();
+
+            if( professor == null )
+                return NotFound();
+
+            return Ok(professor);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Professor professor)
+        {
+            _context.Professores.Add(professor);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
     }
 }
